31f4b16c8a05c85b4609ef6303c7b67671b51be2
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-02-26 15:48:31 -0500

    updated README

--- README.md
+++ README.md
@@ -32,4 +32,10 @@ To uninstall, go to the git directory and run:
 foo@bar:~$ sudo ./uninstall.sh
 ```
 
+# Notes
+
+One of Linux's biggest drawing points is, in my opinion, also one of it's biggest drawbacks: modularity. There are umpteen different distros with as many backends, configurations, and desktop environments. This app was written and tested on elementaryOS 5 Hera, which is based on Ubuntu with the GNOME desktop. As such, it uses a program called gsettings to change the desktop wallpaper. If you are using a similar GNOME-based distro, it will probably work for you. But if you're using something with, say, a KDE desktop then it probably won't, and you'll need to change the part of the script that says "THIS PART IS GNOME SPECIFIC" to allow you to change the wallpaper. I don't have a KDE system installed, and I certainly don't have the time, energy, or patience to test for every Desktop Environment out there. Someday projects like freedesktop.org will give us a comman API to do common DE tasks like change the wallpaper or add icons to the desktop, but Linux is still a long way off in that regard to closed systems like Mac or Windows.
+
+I know there is code out there to test for different DEs and set the wallpaper accordingly, but as I said I don't have any other working Linux setups right now, so if this app doesn't work for you, feel free to fork it, change it, and send a pull request or a DM and I'll look into it.
+
 # -)
--- apod_linux.py
+++ apod_linux.py
@@ -12,14 +12,6 @@ import os
 import subprocess
 import urllib.request
 
-# the url to load JSON from
-url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
-
-# get the JSON and format it
-response = urllib.request.urlopen(url)
-byte_data = response.read()
-data = json.loads(byte_data)
-
 # the hidden dir to store the wallpaper
 home_dir = os.path.expanduser('~')
 pic_dir = (home_dir + '/.apod_linux')
@@ -31,32 +23,40 @@ if not os.path.exists(pic_dir):
     try:
         os.mkdir(pic_dir)
     except OSError as e:
-        print("Could not create directory, exiting...")
+        print("Could not create directory, freaking out...")
         sys.exit(1)
+
+# the url to load JSON from
+url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+
+# get the JSON and format it
+response = urllib.request.urlopen(url)
+byte_data = response.read()
+data = json.loads(byte_data)
 
 # make sure it's an image (sometimes it's a video)
 media_type = data['media_type']
-if 'image' in media_type:
-
-    # get the url to the actual image
-    pic_url = data['hdurl']
-
-    # check the file ext
-    file_ext = pic_url.split('.')[-1]
-
-    # format a new filename
-    file_name = 'wallpaper.' + file_ext
-
-    # download the full picture
-    urllib.request.urlretrieve(pic_url, pic_dir + '/' + file_name)
-
-    # call gsettings to set the wallpaper
-    subprocess.call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri',
-        'file://' + pic_dir + '/' + file_name])
-
-# if it's a video
-else:
-    print('Not an image, exiting...')
+if not 'image' in media_type:
+    print('Not an image, freaking out...')
     sys.exit(1)
+
+# get the url to the actual image
+pic_url = data['hdurl']
+
+# check the file ext
+file_ext = pic_url.split('.')[-1]
+
+# format a new filename
+file_name = ('wallpaper.' + file_ext)
+pic_path = (pic_dir + '/' + file_name)
+
+# download the full picture
+urllib.request.urlretrieve(pic_url, pic_path)
+
+# call gsettings to set the wallpaper
+# N.B. THIS PART IS GNOME SPECIFIC
+cmd = 'gsettings set org.gnome.desktop.background picture-uri file://' + pic_path
+cmd = cmd.split()
+subprocess.call(cmd)
 
 # -)
