4a88635ed07fc866b5d952fd79452511179e3034
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-06 17:59:32 -0500

    updated comments

--- apod_linux.sh
+++ apod_linux.sh
@@ -8,6 +8,8 @@
 #------------------------------------------------------------------------------#
 
 # start the python script and fork it to run in the background
+# N.B. the py script has a 30 second delay to wait for an internet connection,
+# so forking it prevents login from hanging for that 30 seconds
 /usr/bin/python3 /usr/bin/apod_linux.py &
 
 # -)
