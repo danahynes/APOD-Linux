b208189b4ed14d4343fa8b6e8e5024d0735dd28a
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-24 13:47:45 -0400

    remove log file

--- uninstall.sh
+++ uninstall.sh
@@ -22,5 +22,6 @@ sudo rm -rf "/home/${user}/.apod_linux"
 sudo rm -rf /etc/profile.d/apod_linux_login.sh
 sudo rm -rf /lib/systemd/system-sleep/apod_linux_wake.sh
 sudo rm -rf /usr/bin/apod_linux.py
+sudo rm -rf /var/log/apod_linux.log
 
 # -)
