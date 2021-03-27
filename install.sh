7fc5c33064d651d02f8fad8606f2aa9941c7cf1c
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-26 21:35:05 -0400

    deleted all file checks (not needed)

--- install.sh
+++ install.sh
@@ -13,51 +13,25 @@ if [ "${user}" == "root" ]
 then
 
   # DO NOT RUN AS ROOT!!! set-wallpaper will try to delete your hard drive!!!
-  echo 'Do not run as root, it breaks set-wallpaper!'
+  echo 'Do not run install.sh as root, it breaks set-wallpaper!'
   exit 1
 fi
 
 # make the dir to store wallpaper
 mkdir -p "/home/${user}/.apod_linux"
-sudo chown ${user}:${user} "/home/${user}/.apod_linux"
 
 # put current user into a copy of wake script
 touch ./apod_linux_wake2.sh
 sed "s/REPLACE_USER/${user}/g" ./apod_linux_wake.sh > ./apod_linux_wake2.sh
-
-# copy the script for login
-if [ -d "/etc/profile.d" ]
-then
-  sudo cp ./apod_linux_login.sh /etc/profile.d/
-else
-  echo "/etc/profile.d does not exist, freaking out..."
-  exit 1
-fi
-
-# change permissions and owner of apod_linux_wake
 sudo chmod +x ./apod_linux_wake2.sh
 sudo chown root:root ./apod_linux_wake2.sh
 
-# move the script copy for wake
-if [ -d "/lib/systemd/system-sleep" ]
-then
-  sudo mv ./apod_linux_wake2.sh /lib/systemd/system-sleep/apod_linux_wake.sh
-else
-  echo "/lib/systemd/system-sleep does not exist, freaking out..."
-  exit 1
-fi
-
-# copy the script for changing the wallpaper
-if [ -d "/usr/bin" ]
-then
-  sudo cp ./apod_linux.py /usr/bin/
-else
-  echo "/usr/bin does not exist, freaking out..."
-  exit 1
-fi
+# copy the scripts to their locations
+sudo cp ./apod_linux_login.sh /etc/profile.d/
+sudo mv ./apod_linux_wake2.sh /lib/systemd/system-sleep/apod_linux_wake.sh
+sudo cp ./apod_linux.py /usr/bin/
 
 # run the script now (runs as current user)
-#su -s $(/usr/bin/env bash) -c /etc/profile.d/apod_linux_login.sh ${user}
-su -s /bin/bash -c /etc/profile.d/apod_linux_login.sh ${user}
+/etc/profile.d/apod_linux_login.sh
 
 # -)
