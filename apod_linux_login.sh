f1a5927fcdea6ff9db044d5400cd62d2c7a2eebc
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-26 22:32:05 -0400

    minor edits

--- apod_linux_login.sh
+++ apod_linux_login.sh
@@ -9,8 +9,8 @@
 
 # start the python script and fork it to run in the background
 # N.B. the py script has a 30 second delay to wait for an internet connection,
-# so forking it prevents login from hanging for that 30 seconds
-# also downloading the pic may take a minute or two, so don't hang for that
+# so forking it prevents login from hanging for that 30 seconds, also
+# downloading the pic may take a minute or two, so don't hang for that
 /usr/bin/python3 /usr/bin/apod_linux.py &
 
 # -)
