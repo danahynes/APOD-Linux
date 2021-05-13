4e1dad184f921f14333a9070c170c2843f65936f
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-13 17:09:32 -0400

    added some messages while installing

--- install.sh
+++ install.sh
@@ -15,32 +15,50 @@ then
   exit 1
 fi
 
+# show some progress
+sudo echo "Installing APOD_Linux..."
+echo "For license info see the LICENSE.txt file in this directory"
+
 # stop the unlock listener (so we don't run it twice)
-pkill -f "/usr/bin/apod_linux_unlock.sh"
+echo -n "Stopping unlock listener... "
+sudo pkill -f "/usr/bin/apod_linux_unlock.sh"
+echo "Done"
 
 # set the hidden dir where we will store our junk
 INSTALL_DIR="${HOME}/.apod_linux"
 
 # make the dir to store wallpaper and log (as user)
+echo -n "Creating install directory... "
 mkdir -p "${INSTALL_DIR}"
+echo "Done"
 
 # install the conf file (as user)
+echo -n "Copying config file for caption... "
 cp ./apod_linux.conf "${INSTALL_DIR}"
+echo "Done"
 
 # install the uninstaller
+echo -n "Copying uninstaller... "
 cp ./uninstall.sh "${INSTALL_DIR}"
+echo "Done"
 
 # make a log file now in case anyone needs it before the py script runs (the py
 # logging system will create the file if needed, but bash scripts won't)
+echo -n "Creating log file... "
 touch "${INSTALL_DIR}/apod_linux.log"
+echo "Done"
 
 # copy the scripts to their locations (needs admin hence sudo)
+echo -n "Copying scripts to their locations... "
 sudo cp "./apod_linux_login.sh" "/etc/profile.d"
 sudo cp "./apod_linux_unlock.sh" "/usr/bin"
 sudo cp "./apod_linux.py" "/usr/bin"
 sudo cp "./apod_linux_caption.sh" "/usr/bin"
+echo "Done"
 
 # run the script now (as user) (fork and release as child)
+echo -n "Running APOD_Linux now... "
 /etc/profile.d/apod_linux_login.sh & disown
+echo "Done"
 
 # -)
