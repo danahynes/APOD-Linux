c01e0b74f7948c65cb2de679232cdb52bab46386
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-06-10 21:31:30 -0400

    changed name of test wallpaper

--- apod_linux.py
+++ apod_linux.py
@@ -117,7 +117,7 @@ else:
     sys.exit(0)
 
     # NB: this is for testing on days when the APOD is not an image
-    # pic_path = os.path.join(home_dir, 'Downloads/ZodiacalNight.jpg')
+    # pic_path = os.path.join(home_dir, 'Documents/Projects/APOD_Linux/test.jpg')
     # apod_data = {'explanation':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 #-------------------------------------------------------------------------------
