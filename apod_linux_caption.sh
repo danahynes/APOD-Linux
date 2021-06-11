447d074e02724e360b585541931ac3d2e6e7bdbe
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-06-10 21:31:57 -0400

    more comments

--- apod_linux_caption.sh
+++ apod_linux_caption.sh
@@ -7,6 +7,9 @@
 # License : WTFPLv2                                              \          /  #
 #------------------------------------------------------------------------------#
 
+# where everybody lives
+INSTALL_DIR="${HOME}/.apod_linux"
+
 #-------------------------------------------------------------------------------
 # get the stuff from the conf file
 #-------------------------------------------------------------------------------
@@ -14,13 +17,14 @@
 # NB: i know this is a big ugly command but it just strips leading and trailing
 # whitespace and whitespace around the equals sign in case of a typo.
 # generally speaking, the conf file should pass muster as a bash script so keys
-# and values should have no spaces...
-source <(grep = "${HOME}/.apod_linux/apod_linux.conf" | \
+# and values should have no spaces. The conf file will eventually be
+# read/written through a gui which will eliminate those issues, but the file
+# "could" still be written by hand, so i'll probably leave this here.
+source <(grep = "${INSTALL_DIR}/apod_linux.conf" | \
     sed 's/^[ \t]*//g' | sed 's/[ \t]*=[ \t]*/=/g' | sed 's/$[ \t]*//g')
 
-APOD_WANT_CAPTION="${CAPTION:=0}"
-
 # do we even want a caption?
+APOD_WANT_CAPTION="${CAPTION:=0}"
 if [ "${APOD_WANT_CAPTION}" == "0" ]
 then
 
@@ -38,32 +42,48 @@ APOD_CAPT_TEXT="${2}"
 
 # apply required options (and defaults)
 APOD_CAPT_POSITION="${POSITION:=BR}"
-APOD_CAPT_COLOR="${COLOR:=white}"
+APOD_CAPT_COLOR="${COLOR:=rgba(255,255,255,1.0)}"
 APOD_CAPT_BACKGROUND="${BACKGROUND:=rgba(0,0,0,0.75)}"
 
 # position/size variables
 # NB: all values relative to screen size. these values look good on me,
 # 1920x1080. YMMV
-APOD_CAPT_WIDTH="${WIDTH:=500}"                   # width of caption
-APOD_CAPT_FONT_SIZE="${FONT_SIZE:=15}"            # font size
-APOD_CAPT_CORNER_RADIUS="${CORNER_RADIUS:=15}"    # corner rounding
-APOD_CAPT_BORDER="${BORDER:=20}"                  # empty space between text and background bubble
-APOD_CAPT_TOP_PADDING="${TOP_PADDING:=50}"        # account for top bar in elementary
-APOD_CAPT_BOTTOM_PADDING="${BOTTOM_PADDING:=10}"  # don't let caption touch bottom of screen
-APOD_CAPT_SIDE_PADDING="${SIDE_PADDING:=10}"      # don't let caption touch sides of screen
+# width of caption
+APOD_CAPT_WIDTH="${WIDTH:=500}"
+# font size
+APOD_CAPT_FONT_SIZE="${FONT_SIZE:=15}"
+# corner rounding of background
+APOD_CAPT_CORNER_RADIUS="${CORNER_RADIUS:=15}"
+# empty space between text and background
+APOD_CAPT_BORDER="${BORDER:=20}"
+# spacing between caption and top of screen (different than bottom to account
+# for a top bar, if present)
+APOD_CAPT_TOP_PADDING="${TOP_PADDING:=50}"
+# spacing between caption and bottom of screen
+APOD_CAPT_BOTTOM_PADDING="${BOTTOM_PADDING:=10}"
+# spacing between caption and side of screen
+APOD_CAPT_SIDE_PADDING="${SIDE_PADDING:=10}"
 
 # ext is the last field after the last dot (single quotes for awk)
 APOD_EXT=$(echo "${APOD_ORIGINAL_FILE}" | awk -F '.' '{print $NF}')
 
 # temp file names
-APOD_TEXT_IMG="${HOME}/.apod_linux/text.png"
-APOD_BACK_IMG="${HOME}/.apod_linux/back.png"
-APOD_COMB_IMG="${HOME}/.apod_linux/comb.png"
-APOD_MASK_IMG="${HOME}/.apod_linux/mask.png"
-APOD_CAPT_IMG="${HOME}/.apod_linux/capt.png"
-APOD_RESZ_IMG="${HOME}/.apod_linux/apod_linux_wallpaper_resz.${APOD_EXT}"
-APOD_TEMP_IMG="${HOME}/.apod_linux/apod_linux_wallpaper_temp.${APOD_EXT}"
-APOD_LOG_FILE="${HOME}/.apod_linux/apod_linux.log"
+# colored text of a specified font size wrapped to a width on a clear background
+APOD_TEXT_IMG="${INSTALL_DIR}/text.png"
+# the square-cornered background with the specified color
+APOD_BACK_IMG="${INSTALL_DIR}/back.png"
+# the text on the square background
+APOD_COMB_IMG="${INSTALL_DIR}/comb.png"
+# the mask image with rounded corners
+APOD_MASK_IMG="${INSTALL_DIR}/mask.png"
+# the square background text image after applying the round corner mask
+APOD_CAPT_IMG="${INSTALL_DIR}/capt.png"
+# the wallpaper image after zooming
+APOD_RESZ_IMG="${INSTALL_DIR}/apod_linux_wallpaper_resz.${APOD_EXT}"
+# the wallpaper image after adding the caption
+APOD_TEMP_IMG="${INSTALL_DIR}/apod_linux_wallpaper_temp.${APOD_EXT}"
+# the log file - duh!
+APOD_LOG_FILE="${INSTALL_DIR}/apod_linux.log"
 
 #-------------------------------------------------------------------------------
 # time to resize the original image
@@ -126,8 +146,9 @@ TEXT_W=$(identify -format "%[fx:w]" "${APOD_TEXT_IMG}")
 TEXT_H=$(identify -format "%[fx:h]" "${APOD_TEXT_IMG}")
 
 # make new image that is x+y bigger than text image (add border)
-TEXT_WB=$(echo "scale=2;${TEXT_W}+${APOD_CAPT_BORDER}+${APOD_CAPT_BORDER}" | bc)
-TEXT_HB=$(echo "scale=2;${TEXT_H}+${APOD_CAPT_BORDER}+${APOD_CAPT_BORDER}" | bc)
+BORDER=$(echo "scale=0;${APOD_CAPT_BORDER}+${APOD_CAPT_BORDER}" | bc)
+TEXT_WB=$(echo "scale=2;${TEXT_W}+${BORDER}" | bc)
+TEXT_HB=$(echo "scale=2;${TEXT_H}+${BORDER}" | bc)
 
 # create a background for the text
 convert \
